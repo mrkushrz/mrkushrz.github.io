0c20277c9e621cfa9b11611a1828d8c789a74571
mrkushrz <133521730+mrkushrz@users.noreply.github.com>
2023-12-05 14:03:37 +0100

    Update app_flask.py

--- CustomWidget/app_flask.py
+++ CustomWidget/app_flask.py
@@ -1,20 +1,31 @@
-!pip install Flask
-
 from flask import Flask, request, jsonify
-import your_python_script  # Import your script here
+import os
+import Backend  # Import your Backend script
 
 app = Flask(__name__)
 
 @app.route('/generate_commodity_response', methods=['POST'])
 def generate_commodity_response():
-    data = request.json
-    start_date = data['start_date']
-    end_date = data['end_date']
-    commodity = data['commodity']
-    prompt = data['prompt']
-    
-    response = your_python_script.generate_commodity_response(start_date, end_date, commodity, prompt)
-    return jsonify(response)
+    try:
+        # Extract data from request
+        data = request.json
+        start_date = data.get('start_date')
+        end_date = data.get('end_date')
+        commodity = data.get('commodity')
+        prompt = data.get('prompt')
+
+        # Check for missing data
+        if not all([start_date, end_date, commodity, prompt]):
+            return jsonify({"error": "Missing data in request"}), 400
+
+        # Call the function from Backend.py
+        response = Backend.generate_commodity_response(start_date, end_date, commodity, prompt)
+        return jsonify(response)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # Use environment variable for port or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
