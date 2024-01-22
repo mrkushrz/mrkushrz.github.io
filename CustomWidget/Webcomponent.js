328a7d26073baf7e81b95a1087794a1b5b11f264
mrkushrz <133521730+mrkushrz@users.noreply.github.com>
2024-01-22 20:46:07 +0100

    Update Webcomponent.js

--- CustomWidget/Webcomponent.js
+++ CustomWidget/Webcomponent.js
@@ -136,7 +136,7 @@
                 <label for="start-date">Start</label>
                 <input type="date" id="start-date">
                 <button style="display:inline-block" id="analysis-button"> 
-                    <img src="https://cdn-icons-png.flaticon.com/512/12222/12222560.png" style="float:left; margin-right:0.5em">
+                    <img src="https://1000logos.net/wp-content/uploads/2023/02/ChatGPT-Emblem.png" style="float:left; margin-right:0.5em">
                     Analysis
                 </button>
                 <br>
@@ -156,7 +156,7 @@
                 <label for="forecast-date">Forecast End</label>
                 <input type="date" id="forecast-date">
                 <button style="display:inline-block" id="forecast-button"> 
-                    <img src="https://cdn-icons-png.flaticon.com/512/12222/12222560.png" style="float:left; margin-right:0.5em">
+                    <img src="https://1000logos.net/wp-content/uploads/2023/02/ChatGPT-Emblem.png" style="float:left; margin-right:0.5em">
                     Forecast
                 </button>
             </td>
